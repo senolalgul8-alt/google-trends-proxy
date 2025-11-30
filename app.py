b50cd20793dda696feb4f4218a7c8441f3714c63
senolalgul8-alt <senolalgul8@gmail.com>
2025-11-30 03:49:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -112,4 +112,7 @@ def home():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
